0d55885a863148a822af5cfa1613f8a2fc5eebac
Baidadevperso <86443623+Baidadevperso@users.noreply.github.com>
2021-07-21 13:53:29 +0200

    Menu f5
    réglé et près a l'emplois

---
+++ krz_personalmenu/shared/config_webhooks.lua
@@ -0,0 +1,6 @@
+
+Config.Webhooks.LogTransferItem = "https://discord.com/api/webhooks/867369400667471882/JV85RGBYP6QjZcSmBi6RTCbV4pgSb-9Ha8TbWXYOk_Qb27W0lT3aZ3pp5Or-Y8cFntiO"
+Config.Webhooks.LogTransferWeapon = "https://discord.com/api/webhooks/867372832547012628/miqPavDvLdo-Znk2DLFX6ZTtdfF2EEF_n0V0CSNOzPnjxEMumqzPuRuNiqLHe-XzvuqC"
+Config.Webhooks.LogTransferMoney = "https://discord.com/api/webhooks/867373186144665600/OUCtLTjPSIjgZpMsHsrydKYpdptYxjcFTX3C8VQkx5iPdZbsGjK8xgRmTw49LwZqItZE"
+Config.Webhooks.LogAdminMenu = "https://discord.com/api/webhooks/867372909810155520/2wxADtl1F1IH0EMGYsfP7nynXvcPbnzyMSjGJvrWbAqOCOAmMMat-kB0Yfj6usF_c3Vx"
+Config.Webhooks.ModderDetect = "https://discord.com/api/webhooks/867373005625360394/EDlunxQ-DhjJoj46MGyO_SZLjHIUkFIJUWMZBfRoE4JAIghl4H4UHQVbnFJmgL2PDc-M"
